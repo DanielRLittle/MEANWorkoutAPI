60e66b838df4045f426cafb942986c3aa0b570f3
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-29 09:48:22 +0100

    edited problem with param handlers

--- handlers/param-handlers.js
+++ handlers/param-handlers.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, params, next) => {
     for (const param of params) {
-        if (param && !req,query[param]) {
+        if (param && !req.query[param]) {
             return res.status(500).send(`Request failed. Request must include '${param}' param.`);
         }
     }
